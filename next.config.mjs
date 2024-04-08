7d6d97fa0599ced17dc947f1339bfcd2c5b7b1c8
yss <2563526499@qq.com>
2024-04-08 16:38:04 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = "ys";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath,
+  assetPrefix,
 };
 
 export default nextConfig;
